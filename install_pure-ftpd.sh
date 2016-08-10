2c09cd3ab4740c3355f25e81acc2bcde55f6e267
Vex Woo <hap.ddup@gmail.com>
2016-08-09 23:18:43 -0500

    add pure-ftpd installation script

---
+++ install_pure-ftpd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $UID -ne 0 ]; then
+    echo "[*] The script needs root privilege."
+    exit 0
+fi
+
+apt-get install pure-ftpd
+
+groupadd ftpgroup
+useradd -g ftpgroup -d /dev/null -s /etc ftpuser
+pure-pw useradd offsec -u ftpuser -d /ftphome
+pure-pw mkdb
+cd /etc/pure-ftpd/auth/
+ln -s ../conf/PureDB 60pdb
+mkdir -p /ftphome
+chown -R ftpuser:ftpgroup /ftphome/
+/etc/init.d/pure-ftpd restart
