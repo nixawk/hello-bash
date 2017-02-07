91f4f2609b7e25920fcd48a5d93ba83d6a385f9d
nixawk <hap.ddup@gmail.com>
2017-02-07 04:47:51 -0600

    change commands order

--- msf_install_osx.sh
+++ msf_install_osx.sh
@@ -95,8 +95,8 @@ function msfdb_init
 
   # Mac OSX default postgres username: _postgres
   createuser --pwprompt msfuser
-  psql -c "ALTER USER msfuser with ENCRYPTED PASSWORD 'msfpass';" msfdb
   createdb --owner msfuser msfdb
+  psql -c "ALTER USER msfuser with ENCRYPTED PASSWORD 'msfpass';" msfdb
 
   # sudo -u postgres createuser msfuser -dRS
   # sudo -u postgres psql -c "ALTER USER msfuser with ENCRYPTED PASSWORD 'msfpass';" msfdb
